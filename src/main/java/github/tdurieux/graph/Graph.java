3e767a4e0134ba0381cb9689314e77978f623706
Bruno <bruno@Bruno-laptop-2.(none)>
2014-11-19 20:59:31 +0100

    Changing the Graphs representation to guava MultiMap
    The guava MultiMap simplifies all the methods which are focused around edges (even if it makes the getNodes method slightly more complicated, but this method might not even be really needed).
    
    The change does fail one test, however one could argue about whether the behaviour discribed by this test is really wanted.

--- src/main/java/github/tdurieux/graph/Graph.java
+++ src/main/java/github/tdurieux/graph/Graph.java
@@ -1,9 +1,12 @@
 package github.tdurieux.graph;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 
 /**
  * is an abstract representation of a graph
@@ -14,10 +17,10 @@ import java.util.Map;
  *            the node type
  */
 public abstract class Graph<T extends Node> {
-	private Map<T, List<T>> graph;
+	private Multimap<T, T> graph;
 
 	public Graph() {
-		this.graph = new HashMap<T, List<T>>();
+		this.graph = HashMultimap.create();
 	}
 
 	/**
@@ -33,21 +36,11 @@ public abstract class Graph<T extends Node> {
 		if (g2 == null) {
 			return output;
 		}
-		for (T node : this.graph.keySet()) {
-			List<T> g2Current = g2.graph.get(node);
-			if (g2Current != null) {
-				if (!output.graph.containsKey(node)) {
-					output.graph.put(node, new ArrayList<T>());
-				}
-				List<T> current = graph.get(node);
-				for (T edge : current) {
-					if (g2Current.contains(edge)) {
-						output.graph.get(node).add(edge);
-					}
-				}
+		for ( Entry<T, T> edge : graph.entries()) {
+			if(g2.graph.containsEntry(edge.getKey(), edge.getValue())) {
+				output.addEdge(edge.getKey(), edge.getValue());
 			}
 		}
-
 		return output;
 	}
 
@@ -71,11 +64,7 @@ public abstract class Graph<T extends Node> {
 	 * @return the number of edges
 	 */
 	public int numberOfEdges() {
-		int edges = 0;
-		for (T node : this.graph.keySet()) {
-			edges += this.graph.get(node).size();
-		}
-		return edges;
+		return graph.size();
 	}
 
 	/**
@@ -85,15 +74,7 @@ public abstract class Graph<T extends Node> {
 	 *            the node
 	 */
 	public void addEdge(T node1, T node2) {
-		if (!graph.containsKey(node1)) {
-			graph.put(node1, new ArrayList<T>());
-		}
-		if (!graph.get(node1).contains(node2)) {
-			graph.get(node1).add(node2);
-		}
-		if (!graph.containsKey(node2)) {
-			graph.put(node2, new ArrayList<T>());
-		}
+		graph.put(node1, node2);
 	}
 
 	/**
@@ -102,37 +83,23 @@ public abstract class Graph<T extends Node> {
 	 * @param node
 	 */
 	public void removeEdge(T node1, T node2) {
-		if (graph.containsKey(node1)) {
-			graph.get(node1).remove(node2);
-		}
-		if (graph.get(node1).size() == 0) {
-			graph.remove(node1);
-		}
-		for (List<T> children : graph.values()) {
-			if (children.contains(node2)) 
-				return;
-		}
-		graph.remove(node2);
+		graph.remove(node1, node2);
+	}
+	
+	public Set<T> getNodes() {
+		Set<T> Nodes = new HashSet<T>(graph.keySet());
+		Nodes.addAll(graph.values());
+		return Nodes;
 	}
 
-	public List<T> getNodes() {
-		return new ArrayList<T>(graph.keySet());
-	}
-
-	public List<T> getEdges(T node) {
-		List<T> edges = new ArrayList<T>();
-		if (graph.containsKey(node)) {
-			edges.addAll(graph.get(node));
-		}
-		return edges;
+	public Collection<T> getEdges(T node) {
+		return graph.get(node);
 	}
 	
 	public String toDot() {
 		String output = "digraph G {\n";
-		for (T node : this.graph.keySet()) {
-			for (T child : this.graph.get(node)) {
-				output += "\t\"" + node.getName() + "\" -> \"" + child.getName() + "\";\n";
-			}
+		for ( Entry<T, T> edge : graph.entries()) {
+			output += "\t\"" + edge.getKey().getName() + "\" -> \"" + edge.getValue().getName() + "\";\n";
 		}
 		return output + "}\n";
 	}
