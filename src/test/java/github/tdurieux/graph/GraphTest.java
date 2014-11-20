f3a5a4abe78eb3f80af57cdb45ee0cecae321ea5
Bruno Lannoo <brunolannoo@gmail.com>
2014-11-20 11:23:45 +0100

    Added setUp to GraphTest

--- src/test/java/github/tdurieux/graph/GraphTest.java
+++ src/test/java/github/tdurieux/graph/GraphTest.java
@@ -2,112 +2,98 @@ package github.tdurieux.graph;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GraphTest {
 
+	private Graph<NodeElement> g1;
+	private Graph<NodeElement> g2;
+	private NodeElement n1;
+	private NodeElement n2;
+	private NodeElement n3;
+	private NodeElement n4;
+	private NodeElement n5;
+	
+	@Before
+	public void setUp() {
+		g1 = new GraphElement();
+		g2 = new GraphElement();
+		n1 = new NodeElement();
+		n2 = new NodeElement();
+		n3 = new NodeElement();
+		n4 = new NodeElement();
+		n5 = new NodeElement();
+	}
+	
 	@Test
 	public void creation_empty_graph() {
-		Graph<?> g = new GraphElement();
-		assertEquals(0, g.numberOfEdges());
-		assertEquals(0, g.getNodes().size());
-		assertTrue(g.isEmpty());
+		assertEquals(0, g1.numberOfEdges());
+		assertEquals(0, g1.getNodes().size());
+		assertTrue(g1.isEmpty());
 	}
 
 	@Test
 	public void add_edge_to_graph() {
-		Graph<NodeElement> g = new GraphElement();
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
+		g1.addEdge(n1, n2);
 
-		g.addEdge(n1, n2);
-
-		assertEquals(1, g.numberOfEdges());
-		assertEquals(2, g.getNodes().size());
-		assertFalse(g.isEmpty());
+		assertEquals(1, g1.numberOfEdges());
+		assertEquals(2, g1.getNodes().size());
+		assertFalse(g1.isEmpty());
 	}
 
 	@Test
 	public void add_recursive_edge_to_graph() {
-		Graph<NodeElement> g = new GraphElement();
-		NodeElement n2 = new NodeElement();
-
-		g.addEdge(n2, n2);
-		assertEquals(1, g.numberOfEdges());
-		assertEquals(1, g.getNodes().size());
+		g1.addEdge(n2, n2);
+		assertEquals(1, g1.numberOfEdges());
+		assertEquals(1, g1.getNodes().size());
 	}
 
 	@Test
 	public void add_existing_edge_to_graph() {
-		Graph<NodeElement> g = new GraphElement();
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
+		g1.addEdge(n1, n2);
 
-		g.addEdge(n1, n2);
+		assertEquals(1, g1.numberOfEdges());
+		assertEquals(2, g1.getNodes().size());
 
-		assertEquals(1, g.numberOfEdges());
-		assertEquals(2, g.getNodes().size());
-
-		g.addEdge(n1, n2);
-		assertEquals(1, g.numberOfEdges());
-		assertEquals(2, g.getNodes().size());
+		g1.addEdge(n1, n2);
+		assertEquals(1, g1.numberOfEdges());
+		assertEquals(2, g1.getNodes().size());
 	}
 	
 	@Test
 	public void remove_existing_edge_from_graph() {
-		Graph<NodeElement> g = new GraphElement();
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
-
-		g.addEdge(n1, n2);
+		g1.addEdge(n1, n2);
 		
-		g.removeEdge(n1, n2);
+		g1.removeEdge(n1, n2);
 
-		assertEquals(0, g.numberOfEdges());
-		assertEquals(0, g.getNodes().size());
+		assertEquals(0, g1.numberOfEdges());
+		assertEquals(0, g1.getNodes().size());
 	}
 
 	@Test
 	public void remove_existing_edge_with_2_parent_from_graph() {
-		Graph<NodeElement> g = new GraphElement();
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
-		NodeElement n3 = new NodeElement();
-
-		g.addEdge(n1, n2);
-		g.addEdge(n3, n2);
+		g1.addEdge(n1, n2);
+		g1.addEdge(n3, n2);
 		
-		g.removeEdge(n1, n2);
+		g1.removeEdge(n1, n2);
 
-		assertEquals(1, g.numberOfEdges());
-		assertEquals(2, g.getNodes().size());
+		assertEquals(1, g1.numberOfEdges());
+		assertEquals(2, g1.getNodes().size());
 	}
 	
 	@Test
 	public void intersection_null_graph() {
-		Graph<NodeElement> g1 = new GraphElement();
-
 		assertTrue(g1.intersection(null).isEmpty());
 	}
 
 	@Test
 	public void intersection_empty_graph() {
-		Graph<NodeElement> g1 = new GraphElement();
-		Graph<NodeElement> g2 = new GraphElement();
-
 		assertTrue(g1.intersection(g2).isEmpty());
 	}
 
 	@Test
 	public void no_intersection_graph() {
-		Graph<NodeElement> g1 = new GraphElement();
-		Graph<NodeElement> g2 = new GraphElement();
-
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
-		NodeElement n3 = new NodeElement();
-		NodeElement n4 = new NodeElement();
-
 		g1.addEdge(n1, n2);
 		g2.addEdge(n3, n4);
 		assertTrue(g1.intersection(g2).isEmpty());
@@ -115,14 +101,6 @@ public class GraphTest {
 
 	@Test
 	public void intersection_one_node_graph() {
-		Graph<NodeElement> g1 = new GraphElement();
-		Graph<NodeElement> g2 = new GraphElement();
-
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
-		NodeElement n3 = new NodeElement();
-		NodeElement n4 = new NodeElement();
-
 		g1.addEdge(n1, n2);
 		g1.addEdge(n2, n3);
 		g2.addEdge(n3, n4);
@@ -137,15 +115,6 @@ public class GraphTest {
 
 	@Test
 	public void intersection_graph() {
-		Graph<NodeElement> g1 = new GraphElement();
-		Graph<NodeElement> g2 = new GraphElement();
-
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
-		NodeElement n3 = new NodeElement();
-		NodeElement n4 = new NodeElement();
-		NodeElement n5 = new NodeElement();
-
 		g1.addEdge(n1, n2);
 		g1.addEdge(n2, n3);
 		g1.addEdge(n3, n5);
@@ -166,20 +135,12 @@ public class GraphTest {
 
 	@Test
 	public void similarity_empty_graph() {
-		Graph<NodeElement> g1 = new GraphElement();
-		Graph<NodeElement> g2 = new GraphElement();
 		double value = g1.similarity(g2);
-
 		assertEquals(0.0, value, 0);
 	}
 
 	@Test
 	public void similarity_identical_graph() {
-		Graph<NodeElement> g1 = new GraphElement();
-		Graph<NodeElement> g2 = new GraphElement();
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
-
 		g1.addEdge(n1, n2);
 		g2.addEdge(n1, n2);
 		double value = g1.similarity(g2);
@@ -189,14 +150,6 @@ public class GraphTest {
 	
 	@Test
 	public void similarity_distinct_graph() {
-		Graph<NodeElement> g1 = new GraphElement();
-		Graph<NodeElement> g2 = new GraphElement();
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
-		
-		NodeElement n3 = new NodeElement();
-		NodeElement n4 = new NodeElement();
-
 		g1.addEdge(n1, n2);
 		g2.addEdge(n3, n4);
 		double value = g1.similarity(g2);
@@ -206,15 +159,6 @@ public class GraphTest {
 	
 	@Test
 	public void similarity_graph() {
-		Graph<NodeElement> g1 = new GraphElement();
-		Graph<NodeElement> g2 = new GraphElement();
-		
-		NodeElement n1 = new NodeElement();
-		NodeElement n2 = new NodeElement();
-		NodeElement n3 = new NodeElement();
-		NodeElement n4 = new NodeElement();
-		NodeElement n5 = new NodeElement();
-
 		g1.addEdge(n1, n2);
 		g1.addEdge(n2, n3);
 		g1.addEdge(n3, n5);
