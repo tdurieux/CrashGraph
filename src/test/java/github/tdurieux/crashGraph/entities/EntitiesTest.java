2248b318bde640f12409a11b104047e6d5a003a6
Bruno Lannoo <brunolannoo@gmail.com>
2014-11-20 10:41:36 +0100

    Added fixture to EntitiesTest
    Extracted the recurring creations of different reports to a fixture

--- src/test/java/github/tdurieux/crashGraph/entities/EntitiesTest.java
+++ src/test/java/github/tdurieux/crashGraph/entities/EntitiesTest.java
@@ -6,13 +6,24 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Set;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class EntitiesTest {
-
+	
+	private static Report report1524;
+	private static Report report1755;
+	private static Report report1759;
+	
+	@BeforeClass
+	public static void setUpClass() throws IOException {
+		report1524 = Report.openReport("src/main/resource/reports/1524.json");
+		report1755 = Report.openReport("src/main/resource/reports/1755.json");
+		report1759 = Report.openReport("src/main/resource/reports/1759.json");
+	}
+	
 	@Test
-	public void creation_report_1524() throws IOException {
-		Report report1524 = Report.openReport("src/main/resource/reports/1524.json");
+	public void creation_report_1524(){
 		assertEquals(1524,report1524.getBugId());
 		assertEquals(1002766487000l, report1524.getDate().getTime());
 		assertEquals(1524, report1524.getGroupId());
@@ -20,9 +31,7 @@ public class EntitiesTest {
 	}
 	
 	@Test
-	public void comparing_reports_1755_and_1759() throws IOException {
-		Report report1755 = Report.openReport("src/main/resource/reports/1755.json");
-		Report report1759 = Report.openReport("src/main/resource/reports/1759.json");
+	public void comparing_reports_1755_and_1759(){
 		assertEquals(0.55, report1755.getLastTrace().similarity(report1759.getLastTrace()),0.01);
 	}
 	
@@ -36,15 +45,6 @@ public class EntitiesTest {
 	public void get_all_report_Names() {
 		Set<String> reports = Report.getReportNames("src/main/resource/reports/");
 		assertEquals(20, reports.size());
-		System.out.println(reports.toString());
 		assertTrue(reports.contains("src/main/resource/reports/1524.json"));
-	}
-	
-	@Test
-	public void creation_bucket_with_1524() throws IOException {
-		Report report1524 = Report.openReport("src/main/resource/reports/1524.json");
-		Bucket bucket1524 = new Bucket(report1524);
-		assertEquals(1, bucket1524.getReportIds().size());
-		assertTrue(bucket1524.getReportIds().contains(1524));
 	}
 }
